73bf5e9e60c09912c3ed58b315960366ef4544cc
jaguarhkk <abekorir3470@gmail.com>
2025-07-22 15:41:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
+// Serve all files in the current directory
+app.use(express.static(__dirname));
 
-app.use((req, res) => {
-  res.status(404).send("Page not found");
+// Default to index.html for root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Handle all other file requests
+app.get('*', (req, res) => {
+  const filePath = path.join(__dirname, req.path);
+
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).send('Page not found');
+    }
+  });
+});
+
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
