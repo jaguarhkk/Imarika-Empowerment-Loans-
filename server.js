4583baa2eea52d23af227ee7c64fbe25fc0b9f65
jaguarhkk <abekorir3470@gmail.com>
2025-07-22 15:20:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
 
+// Send index.html for any unknown route
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
